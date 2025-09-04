8a2cafb52632632cb9d9ac6dcd915de5eb6dfbc0
Shipovmax <shipov.max@icloud.com>
2025-09-04 18:46:31 +0300

    Удалил все кроме реад ми файлов

--- scripts/run_backend.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ -d ".venv" ]; then
-  source .venv/bin/activate
-fi
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
--- scripts/run_bot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ -d ".venv" ]; then
-  source .venv/bin/activate
-fi
-python bot/bot.py

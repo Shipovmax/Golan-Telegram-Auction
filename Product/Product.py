3f3cdba2fd96ac2ae3d7ee9cfa6123f005317408
Shipov_maxim <Shipov.max@icloud.com>
2025-09-13 12:34:17 +0300

    Чуток правлю

--- Product/Product.py
+++ Product/Product.py
@@ -1,75 +1,71 @@
-
 # Розы
-Rose_name  = "Розы"
-Rose_cost  = 50000
-Rose_many  = 300
+Rose_name = "Розы"
+Rose_cost = 50000
+Rose_many = 300
 Rose_price = 80000
 
 # Пионы
-Pion_name  = "Пионы"
-Pion_cost  = 85000
-Pion_many  = 100
+Pion_name = "Пионы"
+Pion_cost = 85000
+Pion_many = 100
 Pion_price = 150000
 
 # Георгины
-Geo_name  = "Георгины"
-Geo_cost  = 30000
-Geo_many  = 80
+Geo_name = "Георгины"
+Geo_cost = 30000
+Geo_many = 80
 Geo_price = 50000
 
 # Ромашки
-Rom_name  = "Ромашки"
-Rom_cost  = 100000
-Rom_many  = 500
+Rom_name = "Ромашки"
+Rom_cost = 100000
+Rom_many = 500
 Rom_price = 130000
 
 # Лилии
-Lili_name  = "Лилии"
-Lili_cost  = 60000
-Lili_many  = 200
+Lili_name = "Лилии"
+Lili_cost = 60000
+Lili_many = 200
 Lili_price = 95000
 
 # Тюльпаны
-Tulip_name  = "Тюльпаны"
-Tulip_cost  = 40000
-Tulip_many  = 350
+Tulip_name = "Тюльпаны"
+Tulip_cost = 40000
+Tulip_many = 350
 Tulip_price = 65000
 
 # Орхидеи
-Orhid_name  = "Орхидеи"
-Orhid_cost  = 120000
-Orhid_many  = 60
+Orhid_name = "Орхидеи"
+Orhid_cost = 120000
+Orhid_many = 60
 Orhid_price = 200000
 
 # Хризантемы
-Hriz_name  = "Хризантемы"
-Hriz_cost  = 45000
-Hriz_many  = 250
+Hriz_name = "Хризантемы"
+Hriz_cost = 45000
+Hriz_many = 250
 Hriz_price = 70000
 
 # Лаванда
-Lavanda_name  = "Лаванда"
-Lavanda_cost  = 20000
-Lavanda_many  = 400
+Lavanda_name = "Лаванда"
+Lavanda_cost = 20000
+Lavanda_many = 400
 Lavanda_price = 35000
 
 # Нарциссы
-Narc_name  = "Нарциссы"
-Narc_cost  = 55000
-Narc_many  = 150
+Narc_name = "Нарциссы"
+Narc_cost = 55000
+Narc_many = 150
 Narc_price = 90000
 
 # Ирисы
-Iris_name  = "Ирисы"
-Iris_cost  = 70000
-Iris_many  = 120
+Iris_name = "Ирисы"
+Iris_cost = 70000
+Iris_many = 120
 Iris_price = 115000
 
 # Гвоздики
-Gvoz_name  = "Гвоздики"
-Gvoz_cost  = 25000
-Gvoz_many  = 300
+Gvoz_name = "Гвоздики"
+Gvoz_cost = 25000
+Gvoz_many = 300
 Gvoz_price = 45000
-
-
-

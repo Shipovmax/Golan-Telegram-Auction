05cf1be47a4f4d4f78927fa7c0cbd5052a6f80c0
Shipov_maxim <Shipov.max@icloud.com>
2025-09-13 12:00:36 +0300

    Начал выстраивать проект

---
+++ Players.py
@@ -0,0 +1,18 @@
+# Импортируем продукты
+from Product import *
+
+# Игрок 1
+
+players_1_balance = 150000
+players_1_name    = "Ваня"
+players_1_wants   = Pion_name
+players_1_no_wants= Rose_name
+
+# Игрок 2
+players_2_balance = 280000
+players_2_name    = "Анастасия"
+players_2_wants   = Rose_name
+players_2_no_wants= Pion_name
+
+
+

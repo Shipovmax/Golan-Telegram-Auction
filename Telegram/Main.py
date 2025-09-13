4c7b408e40d99ea659e1318cbd937d3ffade8f4a
Shipov_maxim <Shipov.max@icloud.com>
2025-09-13 16:23:10 +0300

    Сделал все в одном коде

--- Telegram/Main.py
+++ Telegram/Main.py
@@ -1,11 +1,17 @@
 # Импортируем библиотеку
 import asyncio
-from aiogram import Bot , Dispatcher , F
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
-from aiogram.filters import Command , CommandStart
+from aiogram.filters import Command, CommandStart
 
 # Импортируем из других файлов
 from Telegram.mutable_variables import Bot_API
+from Players.Players import *
+from Product.Product import *
+
+# Список валидации имен
+with open("names.txt", "r", encoding="utf-8") as f:
+    names_list = [line.strip() for line in f if line.strip()]
 
 # Создаем бота и диспечера
 bot = Bot(token=Bot_API)
@@ -15,7 +21,7 @@ dp = Dispatcher()
 @dp.message(CommandStart)
 async def cmd_start(message: Message):
     await message.answer("Привет! Как тебя зовут ?")
-    
+
 
 async def main():
     await dp.start_polling(bot)

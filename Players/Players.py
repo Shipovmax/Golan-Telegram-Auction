2e1a6aa195dbd1f40229e9bba597eaa6354814a0
Shipov_maxim <Shipov.max@icloud.com>
2025-09-13 12:28:21 +0300

    Прописал все норм

---
+++ Players/Players.py
@@ -0,0 +1,40 @@
+# Импортируем продукты
+from Product.Product import *
+
+# Игрок 1
+players_1_balance = 150000
+players_1_name    = "Ваня"
+players_1_wants   = Pion_name
+players_1_no_wants= Rose_name
+
+# Игрок 2
+players_2_balance = 280000
+players_2_name    = "Анастасия"
+players_2_wants   = Rose_name
+players_2_no_wants= Pion_name
+
+# Игрок 3
+players_3_balance = 200000
+players_3_name    = "Игорь"
+players_3_wants   = Orhid_name
+players_3_no_wants= Rom_name
+
+# Игрок 4
+players_4_balance = 120000
+players_4_name    = "Марина"
+players_4_wants   = Tulip_name
+players_4_no_wants= Geo_name
+
+# Игрок 5
+players_5_balance = 300000
+players_5_name    = "Дмитрий"
+players_5_wants   = Lavanda_name
+players_5_no_wants= Lili_name
+
+# Игрок 6
+players_6_balance = 175000
+players_6_name    = "Светлана"
+players_6_wants   = Iris_name
+players_6_no_wants= Gvoz_name
+
+

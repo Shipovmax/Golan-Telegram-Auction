d46757656e05224ae942d26b622ac71fa398d0ba
Shipov_maxim <Shipov.max@icloud.com>
2025-09-15 06:42:00 +0300

    Дописал комент

--- Telegram/app/heandler.py
+++ Telegram/app/heandler.py
@@ -3,8 +3,10 @@ from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 
+# Импортируем из файлов
 from Players.Players import *
 from Product.Product import *
+
 # Инициализация роутера
 router = Router
 
